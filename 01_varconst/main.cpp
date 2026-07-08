e46abec496b916517d859261f5ff5e7bcb33a0a1
stwi-developer <stwi.developer@gmail.com>
2026-07-08 12:29:08 +0000

    Completed with vaiable and constants

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello from Harsh 1" << endl;
+
+    cout << "Hello from Harsh 2" << endl;
+
+    // cout << "Hello from Harsh 3" << endl;
+
+    int score; //declration
+    score = 10; // initialization or varaible used
+
+    int score2 = 110; // both steps in one step
+    score2 = 200;
+
+    const int uid = 2323232; //how to declare and initialize a constant 
+
+    cout << score << endl;
+    
+    cout << score2 << endl;
+
+
+    return 0;
+}
