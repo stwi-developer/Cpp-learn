5f456325cd1a51a2e2bab3103e97d47aac59927e
stwi-developer <stwi.developer@gmail.com>
2026-07-08 12:32:38 +0000

    Done some changes

---
+++ indepth.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World from Harsh" << endl;
+    // harsh();
+}
+
+// int harsh() {
+//     cout << "This is a function having name Harsh" << endl;
+// }
